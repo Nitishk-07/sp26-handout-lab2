aff26d7041edc4f8f05b812582e0e59bac3febf2
Rasika Bhalerao <rasikabh@users.noreply.github.com>
2026-01-18 13:46:32 -0800

    Specify that students must write signatures
    Updated comments in Calculator class to include method signatures.

--- src/q1.py
+++ src/q1.py
@@ -1,6 +1,7 @@
 """
 Please implement the two classes below.
-Calculator contains comments describing its methods, but you will need to write the documentation.
+Calculator contains comments describing its methods, but you will need to write the method signatures and documentation.
+All numbers are floats (arguments and returns).
 """
 
 import unittest
@@ -8,8 +9,8 @@ import unittest
 class Calculator:
     pass
     # Methods:
-    #   add(a, b) - returns a + b
-    #   subtract(a, b) - returns a - b
-    #   multiply(a, b) - returns a * b
-    #   divide(a, b) - returns a / b (handle division by zero)
+    #   add(a, b) returns a + b
+    #   subtract(a, b) returns a - b
+    #   multiply(a, b) returns a * b
+    #   divide(a, b) returns a / b (or raises ZeroDivisionError)
 
